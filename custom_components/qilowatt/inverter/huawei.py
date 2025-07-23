a551ead74f5f98422bddbd7a2560a3e28331fef8
Andres Säde <andres.sade@datanor.ee>
2025-07-24 00:42:29 +0300

    Implement device hierarchy support for Huawei inverter

--- custom_components/qilowatt/inverter/huawei.py
+++ custom_components/qilowatt/inverter/huawei.py
@@ -1,4 +1,5 @@
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import device_registry as dr, entity_registry as er
 from qilowatt import EnergyData, MetricsData
 
 from .base_inverter import BaseInverter
@@ -10,19 +11,44 @@ class HuaweiInverter(BaseInverter):
         super().__init__(hass, config_entry)
         self.hass = hass
         self.device_id = config_entry.data["device_id"]
-        # We only need entity registry to get device ID - no need to store entity lists
+        self.all_entities = self.collect_device_entities()
+
+    def collect_device_entities(self):
+        """Collect entities from main device and all child devices"""
+        device_registry = dr.async_get(self.hass)
+        entity_registry = er.async_get(self.hass)
+        
+        all_entities = {}
+        
+        # Add main device entities
+        for entity in entity_registry.entities.values():
+            if entity.device_id == self.device_id and not entity.disabled_by:
+                all_entities[entity.entity_id] = entity
+        
+        # Add child device entities (only those with enabled entities)
+        for device in device_registry.devices.values():
+            if device.via_device_id == self.device_id:
+                enabled_count = sum(1 for e in entity_registry.entities.values() 
+                                  if e.device_id == device.id and not e.disabled_by)
+                if enabled_count > 0:  # Only include devices with enabled entities
+                    for entity in entity_registry.entities.values():
+                        if entity.device_id == device.id and not entity.disabled_by:
+                            all_entities[entity.entity_id] = entity
+        
+        return all_entities
 
     def find_entity_state(self, entity_id):
-        """Helper method to find a state by entity_id (for Huawei sensors)."""
-        # Special case for inverter_power_derating which is a number entity
-        if entity_id == "inverter_power_derating" or entity_id == "sensor.inverter_power_derating":
-            result = self.hass.states.get("number.inverter_power_derating")
-            if result:
-                return result
+        """Find entity using dynamic discovery across device hierarchy"""
+        # Special case for number entities
+        if entity_id == "inverter_power_derating":
+            return self.hass.states.get("number.inverter_power_derating")
         
-        # For all other entities, use sensor prefix approach
-        sensor_entity = entity_id if entity_id.startswith("sensor.") else f"sensor.{entity_id}"
-        return self.hass.states.get(sensor_entity)
+        # Search through all collected entities
+        for full_entity_id in self.all_entities:
+            if full_entity_id.endswith(entity_id):
+                return self.hass.states.get(full_entity_id)
+        
+        return None
 
     def get_state_float(self, entity_id, default=0.0):
         """Helper method to get a sensor state as float."""
@@ -87,10 +113,10 @@ class HuaweiInverter(BaseInverter):
     def get_metrics_data(self):
         """Retrieve METRICS data."""
         # Calculate PV Power for each string
-        pv_voltage_1 = self.get_state_float("inverter_pv_1_voltage")
-        pv_current_1 = self.get_state_float("inverter_pv_1_current")
-        pv_voltage_2 = self.get_state_float("inverter_pv_2_voltage")
-        pv_current_2 = self.get_state_float("inverter_pv_2_current")
+        pv_voltage_1 = self.get_state_float("pv_1_voltage")
+        pv_current_1 = self.get_state_float("pv_1_current")
+        pv_voltage_2 = self.get_state_float("pv_2_voltage")
+        pv_current_2 = self.get_state_float("pv_2_current")
         
         pv_power = [
             pv_voltage_1 * pv_current_1,
@@ -102,7 +128,7 @@ class HuaweiInverter(BaseInverter):
         pv_current = [pv_current_1, pv_current_2]
 
         # Load Power and Current
-        inverter_active_power = self.get_state_float("inverter_active_power")
+        inverter_active_power = self.get_state_float("active_power")
         # Get grid power directly from the sensor
         power_meter_active_power = self.get_state_float("power_meter_active_power")
         # Load power is inverter output minus grid power
@@ -113,15 +139,15 @@ class HuaweiInverter(BaseInverter):
         load_current = [0.0, 0.0, 0.0]  # As per payload
 
         # Battery metrics
-        battery_power = [self.get_state_float("batteries_charge_discharge_power")]
-        battery_current = [self.get_state_float("batteries_bus_current")]
-        battery_voltage = [self.get_state_float("batteries_bus_voltage")]
-        battery_soc = self.get_state_int("batteries_state_of_capacity")
+        battery_power = [self.get_state_float("battery_charge_discharge_power")]
+        battery_current = [self.get_state_float("battery_bus_current")]
+        battery_voltage = [self.get_state_float("battery_bus_voltage")]
+        battery_soc = self.get_state_int("battery_state_of_capacity")
         battery_temperature = [self.get_state_float("battery_1_bms_temperature")]
 
         # Inverter metrics
         inverter_status = 2  # As per payload
-        inverter_temperature = self.get_state_float("inverter_internal_temperature")
+        inverter_temperature = self.get_state_float("internal_temperature")
         alarm_codes = [0, 0, 0, 0, 0, 0]  # As per payload
         grid_export_limit = self.get_state_float("inverter_power_derating")
         
